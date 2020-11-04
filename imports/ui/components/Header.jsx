7b160f5c234fe89a3c0f47673ab9a00c1d1149c6
Chris <chris@pc-133-26.customer.ask4.lan>
2020-11-04 15:13:38 +0000

    Changing logo and icon

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -161,8 +161,17 @@ export default class Header extends Component {
         let signedInAddress = getUser();
         return (
             <Navbar color="primary" dark expand="lg" fixed="top" id="header">
-                    <NavbarBrand tag={Link} to="/"><img src="/img/logo-sharering-white.png" className="img-fluid logo"/> <span className="d-none d-xl-inline-block"><T>navbar.siteName</T>&nbsp;</span><Badge color="secondary"><T>navbar.version</T></Badge> </NavbarBrand>                <UncontrolledDropdown className="d-inline text-nowrap">
-                    {/* <NavbarBrand tag={Link} to="/"><img src="/img/logo-sharering-new.png" className="img-fluid logo"/> <span className="d-none d-xl-inline-block">&nbsp;</span><Badge color="secondary"><T>navbar.version</T></Badge> </NavbarBrand>                <UncontrolledDropdown className="d-inline text-nowrap"> */}
+                    {/* <NavbarBrand tag={Link} to="/"><img src="/img/logo-sharering-white.png" className="img-fluid logo"/> <span className="d-none d-xl-inline-block"><T>navbar.siteName</T>&nbsp;</span><Badge color="secondary"><T>navbar.version</T></Badge> </NavbarBrand>                <UncontrolledDropdown className="d-inline text-nowrap"> */}
+                    <NavbarBrand tag={Link} to="/">
+                        <img src="/img/sharering_explorer_icon_white.png" className="img-fluid icon d-block d-xl-none"/>
+                        <span className="d-none d-xl-inline-block logo">
+                            <img src="/img/sharering_explorer_logo_white.png" className="img-fluid logo"/>&nbsp;
+                        </span>
+                        <Badge color="secondary">
+                            <T>navbar.version</T>
+                            </Badge>
+                    </NavbarBrand>
+                    <UncontrolledDropdown className="d-inline text-nowrap">
                     <DropdownToggle caret={(this.state.networks !== "")} tag="span" size="sm" id="network-nav">{Meteor.settings.public.chainId}</DropdownToggle>
                     {this.state.networks}
                 </UncontrolledDropdown>
