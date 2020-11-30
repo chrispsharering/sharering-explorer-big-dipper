b1886926990f41405f5d42b23eead97fd1ca5f7f
Chris <chris@pc-133-26.customer.ask4.lan>
2020-11-30 16:19:09 +0000

    Adding working aggregate function for summing daily total transactions

--- imports/api/transactions/server/methods.js
+++ imports/api/transactions/server/methods.js
@@ -84,5 +84,127 @@ Meteor.methods({
         }
         return false;
 
-    }
+    },
+    'Transactions.txHistory'(){
+        let transactions = Transactions.rawCollection();
+        // let aggregateQuery = Meteor.wrapAsync(collection.aggregate, collection);
+        const address = 'abc123';
+        // var pipeline = [
+        //     {$match:{"address":address}},
+        //     // {$project:{address:1,height:1,exists:1}},
+        //     {$sort:{"height":-1}},
+        //     {$limit:(Meteor.settings.public.uptimeWindow-1)},
+        //     {$unwind: "$_id"},
+        //     {$group:{
+        //         "_id": "$address",
+        //         "uptime": {
+        //             "$sum":{
+        //                 $cond: [{$eq: ['$exists', true]}, 1, 0]
+        //             }
+        //         }
+        //     }
+        //     }];
+        var pipeline = [
+            {$match:{"height":946014}},
+            // {$project:{address:1,height:1,exists:1}},
+            // {$sort:{"height":-1}},
+            // {$limit:(Meteor.settings.public.uptimeWindow-1)},
+            // {$unwind: "$_id"},
+            // {$group:{
+            //     "_id": "$address",
+            //     "uptime": {
+            //         "$sum":{
+            //             $cond: [{$eq: ['$exists', true]}, 1, 0]
+            //         }
+            //     }
+            // }
+            // }
+        ];
+
+        var pipeline2 = 
+        [
+            {
+              $match: {
+                height: {
+                  $gt: 1
+                }
+              }
+            },
+            {
+              $count: "passing_scores"
+            }
+          ];
+
+        var stringToDateConversionStage = {
+            $addFields: {
+               timestamp: { $toDate: "$timestamp" }
+            }
+         };
+
+        var pipeline3 = 
+        [
+            stringToDateConversionStage,
+            {
+                $project:
+                {
+                    date: { $dateToString: { format: "%Y-%m-%d", date: "$timestamp" } },
+                }
+            },
+            {
+                $group: {
+                    _id: "$date",
+                    count: { $sum: 1 }
+                 }
+            },
+            {
+                $addFields: {
+                    createdAt: "$_id"
+                }
+            },
+            // {
+            //   $count: "passing_scores"
+            // }
+          ];
+
+          var pipeline5 = 
+        [
+            // First Stage
+            {
+                $group :
+                {
+                    _id : "$height",
+                    totalSaleAmount: { $sum: { $multiply: [ "$gas_used", "$gas_wanted" ] } }
+                }
+            },
+            // Second Stage
+            {
+                $match: { "totalSaleAmount": { $gte: 100 } }
+            }
+          ];
+        // let result = aggregateQuery(pipeline, { cursor: {} });
+
+        // var pipeline4 = [{
+        //     $project: {
+        //       year: {$year: timestamp},
+        //       month: {$month: timestamp},
+        //       dayOfMonth: {$dayOfMonth: timestamp}
+        //     }
+        //   },
+        //   {
+        //     $group: {
+        //       _id: {
+        //         year: '$year',
+        //         month: '$month',
+        //         dayOfMonth: '$dayOfMonth'
+        //       },
+        //       count: {
+        //         $sum: 1
+        //       }
+        //     }
+        //   }];
+
+        // return Promise.await(transactions.aggregate(pipeline).toArray());
+        return Promise.await(transactions.aggregate(pipeline3).toArray());
+        // return .aggregate()
+    },
 });
