cdc8b4abbb781a8b1b9b62632ae49cf58ce11ae7
Chris <chris@pc-142-143.customer.ask4.lan>
2020-12-21 13:21:42 +0000

    Fixing avatar bug for blocks page

--- imports/ui/components/Avatar.jsx
+++ imports/ui/components/Avatar.jsx
@@ -11,10 +11,11 @@ export default class Avatar extends React.Component {
         if (this.props.profileUrl){
             return (
                 <img src={this.props.profileUrl} alt={this.props.moniker} className={this.props.list?'moniker-avatar-list img-fluid rounded-circle':'img-fluid rounded-circle'} />
-            );    
+            ); 
         }
         else {
-            return <div className={this.props.list?'moniker-avatar-list':''}><Jazzicon diameter={this.props.list?48:100} seed={jsNumberForAddress(this.props.address)} /></div>
+            const jazzIconStyling = { verticalAlign: "middle", paddingRight: "0.4em" };
+            return <span className={this.props.list?'moniker-avatar-list':''} style={jazzIconStyling}><Jazzicon diameter={23} seed={jsNumberForAddress(this.props.address)} /></span>
         }
     }
 }
